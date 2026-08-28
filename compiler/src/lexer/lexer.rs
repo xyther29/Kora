13f371b7806d1a891ce0ce15e9594c4f19ddebda
xyther29 <hritikmaurya24@gmail.com>
2026-08-28 17:38:03 +0530

    feat: tokenize integer literals

--- compiler/src/lexer/lexer.rs
+++ compiler/src/lexer/lexer.rs
@@ -1,3 +1,4 @@
+use crate::lexer::token::TokenKind;
 pub struct Lexer {
     source: Vec<char>,
     position: usize,
@@ -15,4 +16,48 @@ impl Lexer {
     fn advance(&mut self) {
         self.position += 1;
     }
+    pub fn next_token(&mut self) -> TokenKind {
+        match self.current_char() {
+            Some(ch) if ch.is_ascii_digit() => self.read_integer(),
+            None => TokenKind::EOF,
+            _ => {
+                self.advance();
+                TokenKind::EOF
+            }
+        }
+    }
+    fn read_integer(&mut self) -> TokenKind {
+        let mut value = String::new();
+        while let Some(ch) = self.current_char() {
+            if !ch.is_ascii_digit() {
+                break;
+            }
+            value.push(ch);
+            self.advance();
+        }
+
+        let number = value.parse::<i64>().unwrap();
+        TokenKind::Integer(number)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lexes_integer() {
+        let mut lexer = Lexer::new("123");
+
+        let token = lexer.next_token();
+
+        assert_eq!(token, TokenKind::Integer(123));
+    }
+}
+#[test]
+fn lexes_large_integer() {
+    let mut lexer = Lexer::new("123456");
+
+    let token = lexer.next_token();
+
+    assert_eq!(token, TokenKind::Integer(123456));
 }
--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -3,7 +3,7 @@ mod lexer;
 use lexer::lexer::Lexer;
 
 fn main() {
-    let source = "let x = 42;";
-    let _lexer = Lexer::new(source);
-    println!("Lexer created succcessfully");
+    let mut lexer = Lexer::new("123");
+    let token = lexer.next_token();
+    println!("{:?}", token);
 }
