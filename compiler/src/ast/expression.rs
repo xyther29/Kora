5ff427f7b0e6ba887414dc90c4fb71d71ced0f7d
xyther29 <hritikmaurya24@gmail.com>
2026-08-29 21:42:12 +0530

    test: validate AST expression variants

--- compiler/src/ast/expression.rs
+++ compiler/src/ast/expression.rs
@@ -5,12 +5,36 @@ pub enum Expression {
     String(String),
     Identifier(String),
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn creates_integer_expression() {
-        let expr = Expression::Integer(42);
-        assert_eq!(expr, Expression::Integer(42));
+        let expression = Expression::Integer(22);
+
+        assert_eq!(expression, Expression::Integer(22));
+    }
+
+    #[test]
+    fn creates_float_expression() {
+        let expression = Expression::Float(12.1415);
+
+        assert_eq!(expression, Expression::Float(12.1415));
+    }
+
+    #[test]
+    fn creates_string_expression() {
+        let expression = Expression::String("Kora".to_string());
+
+        assert_eq!(expression, Expression::String("Kora".to_string()));
+    }
+
+    #[test]
+    fn creates_identifier_expression() {
+        let expression = Expression::Identifier("age".to_string());
+
+        assert_eq!(expression, Expression::Identifier("age".to_string()));
     }
 }
